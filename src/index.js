2e47bed319d4e0c0b6eddd13c458350e93977a3c
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-13 19:40:58 +0700

    Create getWeather

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './output.css'
 
 console.log('Hello, World!')
+const apiKey = 'bfc564c71d9743fb9c5151258241204'
+
+async function getWeather(location) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log(data)
+  if (data.error) {
+    console.log(data.error.message)
+    return data.error.message
+  }
+  return data
+}
+
+getWeather('')
